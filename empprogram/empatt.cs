f570307e84dccbe1144fd994cf77fd51e153c457
AartiPundalik <aartipundalik68@gmail.com>
2022-12-19 21:03:52 +0530

    UC4 added

---
+++ empprogram/empatt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empprogram
+{
+    public class empatt
+
+    {
+        public static void check()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+                
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent ");
+                
+            }
+            
+
+
+        }
+    }
+}
+
+        
+    
+
